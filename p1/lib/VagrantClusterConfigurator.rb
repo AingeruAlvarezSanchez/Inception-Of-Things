fea187d6a0d6537e1d9fb872f24bcbbe7ec8196f
dugonzal <dugonzal@protonmail.com>
2025-05-30 20:41:12 +0200

    feat: mejoro la obtencion de datos y la moludaridad IAC

--- p1/lib/VagrantClusterConfigurator.rb
+++ p1/lib/VagrantClusterConfigurator.rb
@@ -1,61 +1,87 @@
+
+
 # frozen_string_literal: true
 
+require 'json'
+
 class VagrantClusterConfigurator
-  @path = nil
-  @inventory = nil
-  @playbook = nil
-  @nodes = []
-  @box = 'generic/alpine319'
-  public def initialize(path, inventory = nil, playbook = nil)
-    read_file(path)
-    vagrantfile
+  DEFAULT_BOX       = 'generic/alpine319'
+  DEFAULT_INVENTORY = 'scripts/ansible/inventory.ini'
+  DEFAULT_PLAYBOOK  = 'scripts/ansible/playbook.yml'
+  DEFAULT_SCRIPT    = 'scripts/bootstrap.sh'
+  DEFAULT_PATH      = 'confs/config.json'
+  DEFAULT_VERBOSE   = 'vvv'
+  attr_reader :nodes
+  
+
+  def initialize(path: DEFAULT_PATH, box: DEFAULT_BOX, inventory: DEFAULT_INVENTORY, playbook: DEFAULT_PLAYBOOK, script: DEFAULT_SCRIPT, verbose: DEFAULT_VERBOSE)
+    @path      = path
+    @box       = box
+    @inventory = inventory
+    @playbook  = playbook
+    @script    = script
+    @nodes     = []
+    @box       = box
+    @verbose   = verbose 
+
+    validate_configuration
+    read_file
+    generate
   end
 
-  private def vagrantfile
+  private
+
+  private def validate_configuration
+    validate_file(@path, '.json', 'Path must be a valid JSON file')
+    validate_file(@inventory, '.ini', 'Inventory must be an INI file')
+    validate_file(@playbook, '.yml', 'Playbook must be a YAML file')
+    raise 'Box name cannot be nil or empty' if @box.to_s.strip.empty?
+  end
+
+  private  def validate_file(path, ext, msg)
+    raise "#{msg}: path is nil or empty" if path.nil? || path.strip.empty?
+    raise "#{msg}: file does not exist: #{path}" unless File.exist?(path)
+    raise "#{msg}: invalid extension: #{path}" unless File.extname(path) == ext
+  end
+
+  private def read_file()
+    @nodes = JSON.parse(File.read(@path), symbolize_names: true)
+    raise 'No nodes defined in JSON' if @nodes.empty?
+  end
+
+  private def generate
+    raise 'No nodes defined' if @nodes.empty?
+
     Vagrant.configure('2') do |config|
       config.vm.box = @box
-      config.vm.box_check_update = true 
+      config.vm.box_check_update = true
 
-      raise 'No nodes defined' if @nodes.empty?
       @nodes.each do |node|
-          provision_node(config, node)
-       end
+        provision_node(config, node)
+      end
     end
   end
 
-  private def provision_node(config, node)
+  private  def provision_node(config, node)
     config.vm.define node[:name] do |machine|
       machine.vm.hostname = node[:hostname]
       machine.vm.network 'private_network', ip: node[:network_address]
-
       provision_libvirt(machine, node)
       provision_ansible(machine)
     end
   end
 
-  private  def read_file(path)
-    raise "File not found: #{path}" unless File.exist?(path)
-    @path = File.expand_path(path)
-    @nodes = JSON.parse(
-      File.read(path),
-      symbolize_names: true
-    )
-    raise 'No nodes defined' if @nodes.empty?
-  end
-
-  private def provision_ansible(machine, script = nil, inventory = nil, playbook = nil)
-    machine.vm.provision 'shell', path: 'scripts/bootstrap.sh'
+  private  def provision_ansible(machine, privileged: true)
+    machine.vm.provision 'shell', path: @script, privileged: privileged if @script
     machine.vm.provision 'ansible' do |ansible|
-      ansible.inventory_path = 'scripts/ansible/inventory.ini'
-      ansible.playbook = 'scripts/ansible/playbook.yml'
-      ansible.verbose = 'vvv'
+      ansible.inventory_path = @inventory
+      ansible.playbook = @playbook
+      ansible.verbose = @verbose
+      ansible.become = true if privileged
     end
   end
 
-  private def assert(condition, message)
-    raise message unless condition
-  end
-  private def provision_libvirt(machine, node)
+  private  def provision_libvirt(machine, node)
     machine.vm.provider :libvirt do |lv|
       lv.default_prefix = ''
       lv.memory = node[:memory]
@@ -63,3 +89,4 @@ class VagrantClusterConfigurator
     end
   end
 end
+
