b0cc7c14fdc7676b8925f2626abfd61216745bc3
dugonzal <dugonzal@protonmail.com>
2025-06-25 18:13:11 +0200

    fix p3

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+#
+# sudo apt-get update
+# sudo apt-get install -y docker.io
+#
+# sudo usermod -aG docker "$USER"
+#
+#curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-sudo apt-get update
-sudo apt-get install -y docker.io
+k3d cluster create inception
 
-sudo usermod -aG docker "$USER"
+kubectl create namespace argocd
+kubectl create namespace dev
 
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl get nodes,all,namespaces -o wide
