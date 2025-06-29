5121f2aa2f40a9398f60c0f7e5a993eb1b8e6862
dugonzal <dugonzal@protonmail.com>
2025-06-29 14:38:37 +0200

    feat: script basico para p3, faltaria pasarlo a ansible

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # sudo apt-get update
 # sudo apt-get install -y docker.io
@@ -6,12 +6,30 @@
 # sudo usermod -aG docker "$USER"
 #
 #curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+set -euo pipefail
 
+k3d cluster delete inception
 k3d cluster create inception
 
-kubectl create namespace argocd
 kubectl create namespace dev
+kubectl create namespace argocd
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-kubectl get nodes,all,namespaces -o wide
+kubectl apply -n dev -f ci_di_test/manifest/service.yaml
+kubectl apply -n dev -f ci_di_test/manifest/deployment.yaml
+kubectl apply -n argocd -f ci_di_test/manifest/application.yaml
+
+kubectl wait \
+  --for=condition=available \
+  deployment/argocd-server \
+  -n argocd \
+  --timeout=2m
+
+kubectl -n argocd port-forward svc/argocd-server 8080:443 &
+
+kubectl -n dev port-forward services/svc-wil42 8888:8888 &
+
+kubectl -n argocd get secret argocd-initial-admin-secret \
+  -o jsonpath="{.data.password}" |
+  base64 -d
